46cbe240db8abb01748511df7b81c4366ff83ee0
hikurangi <hikurangi@gmail.com>
2016-10-21 13:43:26 +1300

    test form is working locally but not in deployment

--- components/TestForm.js
+++ components/TestForm.js
@@ -4,13 +4,30 @@ class TestForm extends Component {
 
   constructor (props) {
     super(props)
+    this.state = {
+      userName: '',
+      phoneNumber: ''
+    }
+    // this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleName = this.handleName.bind(this)
+    this.handlePhNum = this.handlePhNum.bind(this)
+  }
+
+  handleName (e) {
+    this.setState({userName: e.target.value})
+    console.log('logged username', this.state.userName)
+  }
+
+  handlePhNum (e) {
+    this.setState({phoneNumber: e.target.value})
+    console.log('logged phoneNumber', this.state.phoneNumber)
   }
 
   render () {
     return (
       <form>
-        <input type="text" id="user-name" placeholder="Name" />
-        <input type="text" id="user-phone" placeholder="Phone Number" />
+        <input type="text" id="user-name" onChange={this.handleName} placeholder="Name" />
+        <input type="text" id="user-phone" onChange={this.handlePhNum} placeholder="Phone Number" />
         <button type="submit" >Submit</button>
       </form>
     )
