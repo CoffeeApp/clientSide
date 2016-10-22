6cb0d0e4bdd78966b28bcec54504cb82406fe265
hikurangi <hikurangi@gmail.com>
2016-10-22 18:00:33 +1300

    searchbar initial commit

--- components/NavBar.js
+++ components/NavBar.js
@@ -20,3 +20,30 @@ const NavBar = () => {
 }
 
 export default NavBar
+
+// import React, { Component } from 'react'
+//
+// class SearchBar extends Component {
+//
+//   constructor (props) {
+//     super(props)
+//   }
+//   handleChange () {
+//     this.props.onUserInput(
+//       this.refs.filterTextInput.value
+//     );
+//   }
+//   render () {
+//     return (
+//       <div>
+//         <input type="text" ref="filterTextInput" name="searchName" placeholder="Search" onChange={this.handleChange.bind(this)}/>
+//       </div>
+//     )
+//   }
+//
+// }
+//
+// // /*<img src="http://www.clker.com/cliparts/e/v/A/R/K/t/transparent-magnifying-glass-md.png" />*/
+// // style={{height:10px}}
+//
+// export default SearchBar
