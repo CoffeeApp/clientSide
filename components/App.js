0d72317e9eeeefb332accf007f5b7d4900891fba
hikurangi <hikurangi@gmail.com>
2016-10-22 18:00:33 +1300

    searchbar initial commit

--- components/App.js
+++ components/App.js
@@ -16,8 +16,8 @@ class App extends React.Component {
 			<div id="wrapper">
 				<NavBar />
 				<div className="dashboard">
-					{/* <SelectCoffee coffees={store.getState().coffees} /> */}
-					<Cart />
+					<SelectCoffee coffees={store.getState().coffees} />
+					{/* <Cart /> */}
 					{/* <SelectCafe /> */}
 				</div>
 			</div>
