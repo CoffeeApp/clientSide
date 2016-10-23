ef6676d367b5d2ee587a7e78a4a04cb2e2ca8623
hikurangi <hikurangi@gmail.com>
2016-10-23 16:34:40 +1300

    adding rebase changes

---
+++ reducers/searchword.js
@@ -0,0 +1,12 @@
+import initialState from '../state'
+
+const searchWord = (state = initialState.searchWord, action) => {
+	switch (action.type) {
+	case 'UPDATE_SEARCHWORD':
+		return action.payload
+	default:
+		return state
+	}
+}
+
+export default searchWord
