c460da9120cefcb37fcd79801ecb70b60647282f
hikurangi <hikurangi@gmail.com>
2016-10-24 15:56:14 +1300

    refactoring complete

--- components/Shop.js
+++ components/Shop.js
@@ -16,10 +16,46 @@ const Shop = ({shop, updateOrder}) => (
 			<div>Website: {shop.website}</div>
 			<div>Rating: {shop.rating}</div>
 			<div>Price: ${shop.total}</div>
-			<div>This store is __ away</div>
+			<div>This store is {shop.distance} away</div>
 			<div className="itemdescription">{shop.description}</div>
 		</div>
 	</div>
 )
 
 export default Shop
+
+// import React, { Component } from 'react'
+//
+// class Cafe extends React.Component {
+//
+// 	constructor(props) {
+// 		super(props)
+// 	}
+//
+// 	render() {
+// 		const { coffee, store, shop, addCoffeeToOrder } = this.props
+// 		return (
+// 			<div
+// 				className="item"
+// 				onClick={() => addCoffeeToOrder(coffee.coffee_id, coffee.type)}
+// 			>
+// 				<img
+// 					alt="coffee image"
+// 					className="itemimage"
+// 					src={shop.image}
+// 				/>
+// 				<div className="itemdetails">
+// 					<div className="itemtitle">{shop.name}</div>
+// 					<div>Phone: {shop.phone}</div>
+// 					<div>Website: {shop.website}</div>
+// 					<div>Rating: {shop.rating}</div>
+// 					<div>TOTAL ORDER PRICE: $</div>
+// 					<div>This store is __ away</div>
+// 					<div className="itemdescription">{shop.description}</div>
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// }
+//
+// export default Cafe
