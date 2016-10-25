8cd3614c0c1988c7ed3f0b68fb7f8708074c3e12
hikurangi <hikurangi@gmail.com>
2016-10-25 18:19:50 +1300

    cafes filter successfully!

--- components/SelectShop.js
+++ components/SelectShop.js
@@ -12,10 +12,13 @@ class SelectShop extends React.Component {
   }
 
   render() {
-    const { shops } = this.props
+    const { shops, searchWord } = this.props
+        let filterResults = shops.filter((shop) => {
+          return shop.shop_name.toLowerCase().includes(searchWord.toLowerCase())
+        })
     return (
       <div className="itemscontainer">
-        {shops.map((shop, index) => {
+        {filterResults.map((shop, index) => {
           return (
             <Shop key={index} shop={shop} {...this.props} />
           )
