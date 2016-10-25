13375b26cf073d2cc6add789fce75bfbf6317cf6
Martin Adams <heymartinadams@users.noreply.github.com>
2016-10-25 21:19:50 +1300

    Minor styling of CSS on shop list.

--- components/Shop.js
+++ components/Shop.js
@@ -16,15 +16,15 @@ const Shop = ({shop, updateOrder}) => (
       className="itemimage"
       src={shop.image}
     />
-    <div className="itemdetails">
-      <div className="itemtitle">{shop.shop_name}</div>
-      <div>Phone: {shop.shop_phone}</div>
-      <div>Website: {shop.website}</div>
-      <div>Rating: {shop.rating}</div>
-      <div>Price: ${shop.total.toFixed(2)}</div>
-      <div>This store is {shop.distance < 1000 ? '' + shop.distance + 'm' : '' + shop.distance/1000 + 'Km'} away</div>
-      <div className="itemdescription">{shop.description}</div>
+    <div className="itemtitle">{shop.shop_name}</div>
+    <div className="itemprice">${shop.total.toFixed(2)}</div>
+    <div className="iteminfo">{shop.rating} of 5 stars</div>
+    <div className="iteminfo">{shop.shop_phone}</div>
+    <div className="iteminfo">{shop.website}</div>
+    <div className="iteminfo">
+      This store is <span className="distance">{shop.distance < 1000 ? '' + shop.distance + 'm' : '' + shop.distance/1000 + 'Km'}</span> away.
     </div>
+    <div className="itemdescription">{shop.description}</div>
   </div>
 )
 
