6c1ef15f98e2e0b4e15ad844a1351beabdadf228
Thomas & Lisa <pair+TJRobson.lisa-seeto@devbootcamp.com>
2016-10-25 15:20:10 +1300

    start reducer testing

--- test/index.js
+++ test/index.js
@@ -1,6 +1,69 @@
-var test = require('tape')
+const test = require('tape')
+const initialState = require('../state')
+const coffees = require('../reducers/coffees').default
+const searchWord = require('../reducers/searchword').default
+const coffeeOrder = require('../reducers/order').default
+
 
 test('testing clientSide', function (t) {
   t.ok(true)
+  t.end()
+})
+
+test('testing searchInput is set from user', function (t) {
+  const expected = 'flat white'
+  const action = {
+    type: 'UPDATE_SEARCHWORD',
+    payload: expected
+  }
+  var actual = searchWord(initialState.searchWord, action)
+  t.equal(actual, expected)
+  t.end()
+})
+
+test('testing that a coffee is being recieved after initial state change', function (t) {
+  var coffee = {
+    "id": 1,
+    "type": "Short Black",
+  }
+  const action = {
+    type: 'RECEIVE_COFFEES',
+    payload: coffee
+  }
+
+  const expected = coffee
+  const actual = coffees(initialState.coffees, action)
+  t.equal(actual, expected)
+  t.end()
+})
+
+test('testing that multiple coffees are being received', function (t) {
+  const initialState =[ { "id": 1, "type": "Short Black"} ]
+  const coffee =[ {"id": 4, "type": "Macchiato"}, {"id": 2, "type": "Flat White"} ]
+  const action = {
+    type: 'RECEIVE_COFFEES',
+    payload: coffee
+  }
+  const expected = coffee
+  const actual = coffees(initialState, action)
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('testing that a coffee can be added to order', function (t) {
+  const order =
+  {
+		coffee_id: 1,
+		type: 'short black',
+		quantity: 1,
+		milk: '',
+		sugar: 0
+	}
+
+  const action = {
+    type: 'ADD_COFFEE_TO_ORDER',
+    payload:
+  }
+  const actual = coffeeOrder()
   t.end()
 })
